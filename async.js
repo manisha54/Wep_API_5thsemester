aaad446efb521a24e2868de1cf1d2224398d355a
manisha <manishachaudhary@gmail.com>
2023-04-07 11:49:28 +0545

    asynchronous function

---
+++ async.js
@@ -0,0 +1,15 @@
+
+console.log("start")
+
+setTimeout(function cb(){
+    console.log("callback1")
+},100)
+
+
+setTimeout(function cb(){
+    console.log("callback2")
+},10)
+
+
+
+console.log("end")
